a645571e6749508fe4f922854ff3dd99a08c9add
Connor Slade <connor@connorcode.com>
2021-09-03 20:11:02 -0400

    📃 Add docs / doctests

--- lib/query.rs
+++ lib/query.rs
@@ -5,8 +5,14 @@ pub struct Query {
     pub(crate) data: Vec<[String; 2]>,
 }
 
+/// Implementation for Query
 impl Query {
     /// Create a new query from a string
+    /// # Example
+    /// ```
+    /// use afire::Query;
+    /// let query = Query::new("?foo=bar&nose=dog");
+    /// ```
     pub fn new(query: &str) -> Query {
         // Remove the leading '?' if it exists
         let mut body = query.to_string();
@@ -18,6 +24,11 @@ impl Query {
     }
 
     /// Create a new Query from a Form POST body
+    /// # Example
+    /// ```
+    /// use afire::Query;
+    /// let query = Query::from_body("foo=bar&nose=dog".to_string());
+    /// ```
     pub fn from_body(body: String) -> Query {
         let mut data = Vec::new();
 
@@ -38,11 +49,23 @@ impl Query {
     }
 
     /// Create a new blank query
+    /// # Example
+    /// ```
+    /// use afire::Query;
+    /// let query = Query::new_empty();
+    /// ```
     pub fn new_empty() -> Query {
         Query { data: Vec::new() }
     }
 
     /// Get a value from a key
+    /// # Example
+    /// ```
+    /// use afire::Query;
+    /// let query = Query::new("?foo=bar&nose=dog");
+    /// 
+    /// assert_eq!(query.get("foo"), Some("bar".to_string()));
+    /// ```
     pub fn get(&self, key: &str) -> Option<String> {
         for i in self.data.clone() {
             if i[0] == key {
@@ -53,6 +76,7 @@ impl Query {
     }
 }
 
+/// Implement the fmt::Display trait for Query
 impl fmt::Debug for Query {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.debug_struct("Query").field("data", &self.data).finish()
