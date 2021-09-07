4f78fb1e7ae7956a36e1a48c0b0f397810936e11
Connor Slade <connor@connorcode.com>
2021-09-07 11:56:40 -0400

    📳 Middleward Example

--- examples/08_middleware.rs
+++ examples/08_middleware.rs
@@ -1,63 +1,37 @@
 use afire::{Header, Method, Response, Server};
 
 // Define middleware that will be executed before the request handler.
-// In afire middleware are run before the normal routes and they can ether
+// In afire middleware is run before the normal routes and they can ether
 // send a response or pass the request to the next middleware / route.
+// Just like routes, middleware is executed in reverse order that they are defined.
+// So the most recently defined middleware is executed first.
+
+// You could use middleware to Log all requests, or to check if a user is logged in, Implement ReteLimiting, add Analytics, etc.
 
 fn main() {
     // Create a new Server instance on localhost port 8080
     let mut server: Server = Server::new("localhost", 8080);
 
-    // Define a route to redirect to another website
+    // Define a basic route
     server.route(Method::GET, "/", |_req| {
-        // Because this is all bout headers I have put the header vector here
-        let headers = vec![
-            // Tell the client what type of data we are sending
-            Header::new("Content-Type", "text/html"),
-            // Tell the client to redirect to another website
-            Header::new("Location", "https://connorcode.com"),
-            // Custom header
-            Header::new("X-Custom-Header", "This is a custom header"),
-        ];
-
-        // Define response body
-        // In this case this should only be seen if the client doesn't support redirects for some reason
-        let text = "<a href=\"https://connorcode.com\">connorcode</a>";
-
-        // The response code of 308 tells the client to redirect to the location specified in the header
-        // Thare are other response codes you can use too
-        // 301 -> Moved Permanently
-        // 302 -> Found
-        // 303 -> See Other
-        // 307 -> Temporary Redirect
-        // 308 -> Permanent Redirect
-        Response::new(308, text, headers)
+        Response::new(
+            200,
+            "Hello World!",
+            vec![Header::new("Content-Type", "text/plain")],
+        )
     });
 
-    // Now to define a route to handle client headers
-    // This will just echo the headers back to the client
-    server.route(Method::GET, "/headers", |req| {
-        let mut body = "".to_string();
+    // Now to add some middleware
+    // For this example we will add a middleware that will print all requests to the console
+    server.every(Box::new(|req| {
+        println!("[{}] {} {}", req.address, req.method, req.path);
 
-        // Get the headers from the request and make a html string
-        for i in req.headers {
-            body += &format!("{:?}<br />", i);
-        }
+        // Return None to forward the request to the next middleware or route
+        None
+    }));
 
-        // Create a response with the headers
-        Response::new(200, &body, vec![Header::new("Content-Type", "text/html")])
-    });
-
-    // Define server wide default headers
-    // These will be send with every response
-    // Default headers also have a higher priority than route specific headers so they will override them
-    server.add_default_header(Header::new(
-        "X-Server-Header",
-        "This is a server wide header",
-    ));
-
-    // You can now goto http://localhost:8080 you should see a redirect to https://connorcode.com
-    // And you can goto http://localhost:8080/headers to see the headers your client sent to the server
+    // You can now goto http://localhost:8080 you should see that the request is printed to the console
+    // It should look something like this: `[127.0.0.1:xxxxx] GET /`
 
     println!(
         "[08] Listening on http://{}:{}",
--- examples/README.md
+++ examples/README.md
@@ -9,8 +9,20 @@
 | 05_header         | Make and Read Headers to send extra data              |
 | 06_error_handling | Catch panics in routes                                |
 | 07_serve_static   | Serve static files from a dir                         |
+| 08_middleware.rs  | Use Middleware to log requests                        |
+
+## 01 - Basic
+Create a basic web server that can serve some static text.
+
+## 02 - Serve File
+Read and server binary files from disk.
+
+In the example a html text file is served but this code would work with images, videos, etc.
+
+## 03 - Routeing
+Learn about routeing priority and add a 404 page.
 
 ### TODO
-- Middleware
+
 - Logger
 - Ratelimater
