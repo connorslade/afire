8c711285a8dabffebddc2b8df9f7fd1a47f367fe
Connor Slade <connor@connorcode.com>
2021-12-20 13:56:10 -0500

    🌀 Update Content types

--- lib/extensions/content_type.rs
+++ lib/extensions/content_type.rs
@@ -2,22 +2,14 @@
 pub enum Content {
     /// HTML - `text/html`
     HTML,
-    /// CSS - `text/css`
-    CSS,
-    /// JS - `application/javascript`
-    JS,
-    /// PNG - `image/png`
-    PNG,
-    /// JPEG - `image/jpeg`
-    JPG,
-    /// GIF - `image/gif`
-    GIF,
-    /// ICO - `image/x-icon`
-    ICO,
-    /// SVG - `image/svg+xml`
-    SVG,
     /// TXT - `text/plain`
     TXT,
+    /// CSV - `text/csv`
+    CSV,
+    /// JSON - `application/json`
+    JSON,
+    /// XML - `application/xml`
+    XML,
     /// Custom Content Type
     Custom(String),
 }
@@ -27,61 +19,12 @@ impl Content {
     pub fn as_type(&self) -> String {
         match self {
             Content::HTML => "text/html",
-            Content::CSS => "text/css",
-            Content::JS => "application/javascript",
-            Content::PNG => "image/png",
-            Content::JPG => "image/jpeg",
-            Content::GIF => "image/gif",
-            Content::ICO => "image/x-icon",
-            Content::SVG => "image/svg+xml",
             Content::TXT => "text/plain",
+            Content::CSV => "text/csv",
+            Content::JSON => "application/json",
+            Content::XML => "application/xml",
             Content::Custom(i) => i,
         }
         .to_owned()
     }
 }
-
-// AAC,
-// AVI,
-// BIN,
-// BMP,
-// BZ,
-// BZ2,
-// CDA,
-// CSV,
-// EPUB,
-// GZ,
-// ICS,
-// JAR,
-// JSON,
-// JSONLD,
-// MIDI,
-// MID,
-// MJS,
-// MP3,
-// MP4,
-// MPEG,
-// OGA,
-// OGV,
-// OGX,
-// OPUS,
-// OTF,
-// PDF,
-// RAR,
-// RTF,
-// SH,
-// SWF,
-// TAR,
-// TIF,
-// TIFF,
-// TS,
-// TTF,
-// WAV,
-// WEBA,
-// WEBM,
-// WEBP,
-// WOFF,
-// WOFF2,
-// XHTML,
-// XML,
-// ZIP
